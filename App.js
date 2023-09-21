9863a9250bfb3c2ddd7fbdbf35530a74cfa4d13a
SUJIT SWAIN <90376564+Sujitswain@users.noreply.github.com>
2023-09-21 12:59:38 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MemeGenerator from './components/MemeComponent'
+
+function App() {
+  return (
+    <div>
+        <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
